899d2f26dd33b50d7eb9d6f5e38b2ca6ae55bb29
Gigabite1277 <neiltraining2024@gmail.com>
2025-03-26 23:11:13 +0000

    Working on random card selection for the CPU player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
 var c = setInterval(showclock,1000)
  function showclock(){
- var seconds = document.getElementById('countdown').textContent;
+ var seconds = document.getElementById("countdown").textContent;
    seconds--;
- document.getElementById('countdown').innerHTML=seconds;
+ document.getElementById("countdown").innerHTML=seconds;
    if(seconds==0)
      {
      clearInterval(c);
-       document.getElementById('countdown').innerHTML="GO!";
+       document.getElementById("countdown").innerHTML="GO!";
      }
  }
  
- document.getElementById('countdownaudio').play();
+ document.getElementById("countdownaudio").play();
  setTimeout(timer, 6000);
  
  function timer() {
   document.getElementById("countdown").innerHTML = "";
   //document.getElementById("hudwindow").innerHTML = wtbuilding650.png;
 }
-document.getElementById("hudwindow").style.backgroundImage = 'url(assets/images/wtbuilding650.png)';
+document.getElementById("hudwindow").style.backgroundImage = "url(assets/images/wtbuilding650.png)";
 
-document.getElementById('militarydrums').play();
+document.getElementById("militarydrums").play();
+
+
 
 
 //document.getElementById("hudwindow").src = "wtbuilding650.png";
@@ -29,4 +31,11 @@ document.getElementById('militarydrums').play();
 
 
 
+var cpudeck = ['2_of_clubs', '2_of_diamonds', '2_of_hearts', '2_of_spades', '3_of_clubs', 'oranges'];
+
+function gamestart() {
+cpudeck[Math.floor(Math.random()*cpudeck.length)];
+document.getElementById("cpudeck").src = ("assets/images/cpudeck");
+    
+}
 
