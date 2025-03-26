af5ef0d6337f9af557cbba26f1038663bbda3a48
Gigabite1277 <neiltraining2024@gmail.com>
2025-03-26 17:01:28 +0000

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,16 @@ var c = setInterval(showclock,1000)
  setTimeout(timer, 6000);
  
  function timer() {
-  document.getElementById("countdown").innerHTML = ""
+  document.getElementById("countdown").innerHTML = "";
+ // document.getElementById("hudwindow").innerHTML = wtbuilding650.png;
 }
-document.getElementById("wtbuilding").src = "assets/images/wtbuilding650.png";
+let image = wtbuilding
+
+//document.getElementById("hudwindow").src = "wtbuilding650.png";
+//document.getElementById("countdown").src = "wtbuilding650.png";
+//document.getElementById("countdown").innerHTML = wtbuilding650.png;
+//document.getElementById("hudwindow").src = ("assets/images/wtbuilding650.png");
+
+
+
+
