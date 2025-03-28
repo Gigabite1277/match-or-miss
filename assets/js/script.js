aaea5a5ae3eeb4b123230112629e01b58daf4760
Gigabite1277 <neiltraining2024@gmail.com>
2025-03-28 15:08:33 +0000

    General Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,22 +17,33 @@ var c = setInterval(showclock,1000)
   document.getElementById("countdown").innerHTML = "";
   //document.getElementById("hudwindow").innerHTML = wtbuilding650.png;
 }
+
+//HUD background image
 document.getElementById("hudwindow").style.backgroundImage = "url(assets/images/wtbuilding650.png)";
 
 document.getElementById("militarydrums").play();
 
-//Newly made code
-var cpudeck
+//card play sound
 
-var cpudeck =  ["assets/images/cpudeck/2_of_hearts.png", "assets/images/cpudeck/2_of_spades.png","assets/images/cpudeck/3_of_diamonds.png","assets/images/cpudeck/4_of_clubs.png","assets/images/cpudeck/4_of_diamonds.png" ]
 
-document.getElementById("cpudeck").onclick = function() {gamestart()};
+let cpudeck
+
+cpudeck =  ["assets/images/cpudeck/2_of_hearts.png", "assets/images/cpudeck/2_of_spades.png","assets/images/cpudeck/3_of_diamonds.png","assets/images/cpudeck/4_of_clubs.png","assets/images/cpudeck/4_of_diamonds.png" ]
 
 function gamestart()  {
-//Math.floor(Math.random(cpudeck) * 5);
-document.getElementById("cpudeck").src = "cpudeck";
-//document.getElementById("cpudeck").innerHTML = "4_of_clubs.png";
+  
+document.getElementById("cpudeck").onclick = function() {gamestart()};
+Math.floor(Math.random(cpudeck) * 5);
+document.getElementById("cpudeck").src = "assets/images/cpudeck/2_of_spades.png";
 }
 
 
+//New Code
+let cardnoise
+
+function  cardnoise () {
+document.getElementById("cardnoise1").onclick = function() {cardnoise()};
+document.getElementById("cardnoise1").play(cardnoise.wav);
+}
+//document.getElementById("cpudeck").innerHTML = "4_of_clubs.png";
  
