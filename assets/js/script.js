96592e1b6618be8690d13fa2846b7c9390045017
Gigabite1277 <neiltraining2024@gmail.com>
2025-04-01 17:22:19 +0000

    Just can not get this picture to change randomly, tried everything

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,10 +28,6 @@ document.getElementById("hudwindow").style.backgroundImage = "url(assets/images/
 document.getElementById("militarydrums").play();
 
 
-
-
-
-
 function playAudio() { 
   var x = document.getElementById("cardnoise1"); 
   x.play(); 
@@ -44,23 +40,19 @@ function playAudio() {
 
 
 function cpuplay()
-
 {
+var cpuplay = cpudeck1
 
-document.getElementById("cpudeck").onclick = function() {cpuplay()};  
-//var imageArray = ["images/carddeck/2_of_clubs.png","carddeck/2_of_diamonds.png","images/carddeck/2_of_hearts.png","images/carddeck/2_of_spades.png"];
-//var randomPic = Math.floor(Math.random() *imageArray.length);
-//document.getElementById("cpudeck").src = imageArray[randomPic];
+document.getElementById("cpubutton").onclick = function(){cpuplay()};  
+//var imageArray = ["2_of_clubs.png","carddeck/2_of_diamonds.png","2_of_hearts.png","2_of_spadesrandomPic = cpudeck = Math.floor(Math.random() *imageArray.length);]
+document.getElementById("cpudeck1").src = "assets/images/carddeck/2_of_diamonds.png";
 //document.getElementById("cpudeck").innerHTML = imageArray[randomPic];
- document.getElementById("cpudeck").innerHTML = "assets/images/carddeck/2_of_diamonds.png";
+  //document.getElementById("cpudeck").innerHTML = "images/carddeck/2_of_diamonds.png";
 }
-//const randompic = Math.floor(Math.random() *5);
-//document.getElementById("cpudeck").src = "assets/images/carddeck/3_of_spades.png";
+//const randompic = Math.floor(Math.random() *5)/document.getElementById("cpudeck").src = "assets/images/carddeck/3_of_spades.png";
  //imagearray = ["2_of_diamonds.png","2_of_hearts.png","4_of_spades","5_of_Clubs"]
- //document.getElementById("cpudeck").innerHTML = 4_of_spades.png;
-
-
-
+//document.getElementById("cpudeck").innerHTML = "assets/images/carddeck/2_of_diamonds.png";
+//}
 
 
 
