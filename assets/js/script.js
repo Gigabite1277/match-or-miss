d2389614fc053d20327f0c84950e004bd1b4cc85
Gigabite1277 <neiltraining2024@gmail.com>
2025-04-15 20:56:16 +0000

    Still unable to get card comparisson working

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ var c = setInterval(showclock,1000)
 
 //HUD background image
 
-document.getElementById("hudwindow").style.backgroundImage = "url(assets/images/wtbuilding650.png)";
+document.getElementById("hudwindow").style.backgroundImage = "url(assets/images/greenhud650.png)";
 
 //Military drums - Background soundtrack.
 document.getElementById("militarydrums").play();
@@ -43,10 +43,7 @@ document.getElementById("cardnoise1").play();
 document.getElementById("cpubutton").onclick = function() {cpuplay()};
 
 function cpuplay(random, random1, random2, random3) {
-//deck = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
-//deck1 = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
-//deck2 = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
-//deck3 = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
+
 deck = ["assets/images/1.png","assets/images/2.png","assets/images/3.png","assets/images/4.png"];
 deck1 = ["assets/images/1.png","assets/images/2.png","assets/images/3.png","assets/images/4.png"];
 deck2 = ["assets/images/1.png","assets/images/2.png","assets/images/3.png","assets/images/4.png"];
@@ -59,8 +56,12 @@ document.getElementById("cpudeck1").src = deck[random];
 document.getElementById("p1deck1").src = deck1[random1];
 document.getElementById("p1deck2").src = deck2[random2];
 document.getElementById("p1deck3").src = deck3[random3];
-//This code shows the card for 1 second before being hidden although data should be available to the CPU to run a comparison with the other P1 deck
 
+
+/**
+ *This code shows the card for 1 second before being hidden although data should be available 
+ *to the CPU to run a comparison with the other P1 deck
+ */
 setInterval(cardHidden, 700);
 function cardHidden(){
 document.getElementById("cpudeck1").style.visibility = "hidden";
@@ -72,19 +73,25 @@ document.getElementById("cpudeck1").style.visibility = "hidden";
 
 /**
  * Onclick commands for the
- * the returned calculateCorrectAnswer array
+ * the returned calculateCorrectAnswer array.
  */
 document.getElementById("p1deck1").onclick = function() {cardCheck1()};
 document.getElementById("p1deck2").onclick = function() {cardCheck2()};
 document.getElementById("p1deck3").onclick = function() {cardCheck3()};
 
-function cardCheck1() {
-var cpudeck1
-var p1deck1
-var random
-var random1
-random = document.getElementById("cpudeck1").value;
-random1 = document.getElementById("p1deck1").value;
+
+
+/**
+ * Code for the ther cardCheck function to check if cpudeck1(random) is the same as 
+ * the p1deck1, p1deck2, p1deck3.
+ * There are 3 cardCheck functions, as the human has three decks to make a select one
+ * card from.
+ */
+function cardCheck1(random, random1) {
+
+random = document.getElementById("cpudeck1").innerHTML;
+random1 = document.getElementById("p1deck1").innerHTML;
+
   
 if (cpudeck1 == p1deck1){
     document.getElementById("p1window").innerHTML = "DECK 1 ELIMINATED!!!";
@@ -93,26 +100,24 @@ if (cpudeck1 == p1deck1){
   document.getElementById("p1window").innerHTML = "TARGET MISSED!!!";
 }
 } 
-function cardCheck2() {
-var cpudeck1
-var p1deck2
+function cardCheck2(random, random2) {
+
 
 random = parseInt(document.getElementById("cpudeck1").innerHTML);
 random2 = parseInt(document.getElementById("p1deck2").innerHTML);
-if (cpudeck1 === p1deck2){
+if (random === random2){
     document.getElementById("p1window").innerHTML = "DECK 2 ELIMINATED!!!";
     document.getElementById("cardnoise1").play();
 } else {
   document.getElementById("p1window").innerHTML = "TARGET MISSED!!!";
 }
 }
-function cardCheck3() {
-  var cpudeck1//CHANGED VARIABLES TO CPUDECK1 AND P1DECK1
-  var p1deck3
+function cardCheck3(random, random3) {
+  
   
   random = parseInt(document.getElementById("cpudeck1").innerHTML);
   random3 = parseInt(document.getElementById("p1deck3").innerHTML);
-  if (cpudeck1 === p1deck3){
+  if (random === random3){
       document.getElementById("p1window").innerHTML = "DECK 3 ELIMINATED!!!";
       document.getElementById("cardnoise1").play();
   } else {
@@ -132,3 +137,8 @@ function cardCheck3() {
 //parseInt(document.getElementById("p1deck1").value);
 //let p1deck1 = random === random1;
 //let cpudeck1 = random === random1;
+
+//deck = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
+//deck1 = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
+//deck2 = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
+//deck3 = ["assets/images/carddeck/3_of_spades.png","assets/images/carddeck/2_of_hearts.png","assets/images/carddeck/4_of_spades.png","assets/images/carddeck/5_of_clubs.png"];
