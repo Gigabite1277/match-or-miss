caf612f53244c14672a1a1742857449e91947656
Gigabite1277 <neiltraining2024@gmail.com>
2025-05-05 21:20:10 +0000

    General commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,10 +102,10 @@ document.getElementById("p1deck3").onclick = function() {cardCheck3()};
  * card from.
  */
 
-{
-var health = 6;
-}
-function cardCheck1(random, random1, health) {
+
+
+
+function cardCheck1(random, random1) {
   
 random = document.getElementById("cpudeck1").innerHTML = randomValue();
 random1 = document.getElementById("p1deck1").innerHTML = randomValue1();
@@ -119,13 +119,13 @@ if (random === random1){
 } else {
   document.getElementById("status").innerHTML = "MISS";
   document.getElementById("miss-sound").play();
-  //document.getElementById("health").innerHTML = healthDeduct(); 
-  document.getElementById("health").innerHTML = --health;
+  document.getElementById("health").innerHTML = healthDeduct(); 
+  //document.getElementById("health").innerHTML = --health;
   
   }
 }
 
-function cardCheck2(random, random2, health) {
+function cardCheck2(random, random2) {
  
   random = document.getElementById("cpudeck1").innerHTML = randomValue();
   random2 = document.getElementById("p1deck2").innerHTML = randomValue2();
@@ -140,13 +140,13 @@ function cardCheck2(random, random2, health) {
   } else {
     document.getElementById("status").innerHTML = "MISS";
     document.getElementById("miss-sound").play();
-    //document.getElementById("health").innerHTML = healthDeduct();
-    document.getElementById("health").innerHTML = --health;
+    document.getElementById("health").innerHTML = healthDeduct();
+   // document.getElementById("health").innerHTML = --health;
     
     } 
    
   } 
-  function cardCheck3(random, random3, health) {
+  function cardCheck3(random, random3) {
    
     random = document.getElementById("cpudeck1").innerHTML = randomValue();
     random3 = document.getElementById("p1deck3").innerHTML = randomValue3();
@@ -161,8 +161,8 @@ function cardCheck2(random, random2, health) {
   else {
       document.getElementById("status").innerHTML = "MISS";
       document.getElementById("miss-sound").play();
-      //document.getElementById("health").innerHTML = healthDeduct();
-      document.getElementById("health").innerHTML = --health;
+      document.getElementById("health").innerHTML = healthDeduct();
+      //document.getElementById("health").innerHTML = --health;
       
                 }
                
@@ -175,58 +175,17 @@ document.getElementById("cpudeck1").style.visibility = "";
 
 
 
-
-//{
- 
-  //} 
-
-//function healthDeduct() {
-     //if (health > 0)
-      //health -= 1;
-            //document.getElementById("health").innerHTML = health;
+function healthDeduct(health) {
+  var health = 1;
+     if (health > 0)
+      --health;
+            document.getElementById("health").innerHTML =  health;
             
-        //if (health === 0) {
-            //document.getElementById("status").innerHTML = "Game Over";
+        if (health === 0) {
+            document.getElementById("status").innerHTML = "Game Over";
             // Additional logic to end the game
-        //}
-    //}
-
-
-//
-  //if (health > 0) {
-    //  health -= 1;
-      //document.getElementById("health").innerHTML = --health;
-      //if (health === 0) {
-          //document.getElementById("status").innerHTML = "Game Over";
-          // Additional logic to end the game
-      //}
-  //}
-
-
- 
-
-
-
-
-//function healthDeduct(){
-
-  //for (let health = 6; health > 0; health) {
-  //document.getElementById("health").innerHTML = --health;
-  //health = math.subtract, (1, health);
+        }
+    }
   
-//}
-//} 
   
 
-
-
-
-//parseInt(document.getElementById("cpudeck1").value);
-//parseInt(document.getElementById("p1deck1").value);
-//let p1deck1 = random === random1;
-//let cpudeck1 = random === random1;
-
-//deck = ["assets/images/newdeck/2_of_diamonds.png","assets/images/newdeck/3_of_diamonds.png","assets/images/newdeck/3_of_hearts.png","assets/images/newdeck/10_of_diamonds.png","assets/images/newdeck/10_of_diamonds.png", "assets/images/newdeck/ace_of_spades2.png","assets/images/newdeck/jack_of_clubs2.png","assets/images/newdeck/jack_of_diamonds2.png","assets/images/newdeck/king_of_clubs2.png", "assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/queen_of_hearts2.png"];
-//deck1 = ["assets/images/newdeck/2_of_diamonds.png","assets/images/newdeck/3_of_diamonds.png","assets/images/newdeck/3_of_hearts.png","assets/images/newdeck/10_of_diamonds.png","assets/images/newdeck/10_of_diamonds.png", "assets/images/newdeck/ace_of_spades2.png","assets/images/newdeck/jack_of_clubs2.png","assets/images/newdeck/jack_of_diamonds2.png","assets/images/newdeck/king_of_clubs2.png", "assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/queen_of_hearts2.png"];
-//deck2 = ["assets/images/newdeck/2_of_diamonds.png","assets/images/newdeck/3_of_diamonds.png","assets/images/newdeck/3_of_hearts.png","assets/images/newdeck/10_of_diamonds.png","assets/images/newdeck/10_of_diamonds.png", "assets/images/newdeck/ace_of_spades2.png","assets/images/newdeck/jack_of_clubs2.png","assets/images/newdeck/jack_of_diamonds2.png","assets/images/newdeck/king_of_clubs2.png", "assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/queen_of_hearts2.png"];
-//deck3 = ["assets/images/newdeck/2_of_diamonds.png","assets/images/newdeck/3_of_diamonds.png","assets/images/newdeck/3_of_hearts.png","assets/images/newdeck/10_of_diamonds.png","assets/images/newdeck/10_of_diamonds.png", "assets/images/newdeck/ace_of_spades2.png","assets/images/newdeck/jack_of_clubs2.png","assets/images/newdeck/jack_of_diamonds2.png","assets/images/newdeck/king_of_clubs2.png", "assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/king_of_diamonds2.png","assets/images/newdeck/queen_of_hearts2.png"];
